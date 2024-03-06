ae299c1bd89d9469cd14c89250641600591ff829
Sara Bianchi <sara.bianchi@redturtle.it>
2024-03-06 01:05:08 +0100

    chore: updated audio block with transcript

--- src/components/Blocks/AudioBlock/Body.jsx
+++ src/components/Blocks/AudioBlock/Body.jsx
@@ -3,12 +3,21 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Container } from 'design-react-kit';
 import { flattenToAppURL } from '@plone/volto/helpers';
 import { getContent } from '@plone/volto/actions';
+import { defineMessages, useIntl } from 'react-intl';
 
 /* STYLE */
 import 'volto-wildcard-media/components/Blocks/AudioBlock/AudioBlock.scss';
 
+const messages = defineMessages({
+  transcript_label: {
+    id: 'transcript_label',
+    defaultMessage: 'Trascrizione',
+  },
+});
+
 const Body = (props) => {
   const dispatch = useDispatch();
+  const intl = useIntl();
 
   const audioURL =
     props.data?.audio?.length > 0 ? props.data.audio[0]['@id'] : null;
@@ -27,9 +36,22 @@ const Body = (props) => {
 
   return (
     <Container className="px-0">
+      {/* Title */}
       {props.data?.title && <h2 className="mb-4">{props.data.title}</h2>}
+      {/* Audio */}
       {data?.audio_file?.download && (
         <audio className="mb-4" controls src={data.audio_file.download}></audio>
+      )}
+      {/* Transcript */}
+      {data?.transcript?.data && data?.transcript?.data !== '<p><br></p>' && (
+        <div className="transcript mb-4">
+          <p className="mb-0">
+            <strong>{intl.formatMessage(messages.transcript_label)}</strong>
+          </p>
+          <span
+            dangerouslySetInnerHTML={{ __html: data.transcript.data }}
+          ></span>
+        </div>
       )}
     </Container>
   );
